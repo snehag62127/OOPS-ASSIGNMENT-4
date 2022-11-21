f80c72815dfa046e38bdb8eb3a649a5ee3b2b3e2
snehag62127 <118705053+snehag62127@users.noreply.github.com>
2022-11-21 21:07:38 +0530

    wap of nested class

---
+++ nested.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class Host
+{
+public:
+ class Nested
+ {
+ public:
+ void PrintMe()
+ {
+ cout << "Printed!\n";
+ }
+ };
+};
+int main()
+{
+ Host::Nested foo;
+ foo.PrintMe();
+ return 0;
+}
